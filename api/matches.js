3865b961b140b57b5f4f92d47ffbd5ae4d3c2906
Marcio Barrios <marciobarrios@gmail.com>
2025-04-02 00:44:47 +0200

    Add 3h cache

--- api/matches.js
+++ api/matches.js
@@ -11,5 +11,9 @@ export default async function handler(req, res) {
   );
 
   const data = await response.json();
+
+  // 3h cache on edge/CDN
+  res.setHeader('Cache-Control', 's-maxage=10800, stale-while-revalidate');
+
   res.status(200).json(data);
 }
